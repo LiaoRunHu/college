bade0e7003b58890f679a2237a1bc7a9f41d8a21
Aofeng <1094096981@qq.com>
2021-03-03 13:34:33 +0800

    555

--- build.sh
+++ build.sh
@@ -13,34 +13,34 @@ docker run --rm \
   -v $proj_home:/usr/src/mymaven \
   -w /usr/src/mymaven $img_mvn mvn clean package -U -Pdev -DskipTests
 
-sudo mv $proj_home/common/service_base/target/service_base-*.jar $proj_home/common/service_base/target/demo.jar # 兼容所有sh脚本
+sudo mv $proj_home/common/service_base/target/service_base-0.0.1-SNAPSHOT.jar $proj_home/common/service_base/target/demo.jar # 兼容所有sh脚本
 docker build -t aofeng/common .
 
-sudo mv $proj_home/infrastructure/api_gateway/target/api_gateway-*.jar $proj_home/infrastructure/api_gateway/target/demo.jar # 兼容所有sh脚本
+sudo mv $proj_home/infrastructure/api_gateway/target/api_gateway-0.0.1-SNAPSHOT.jar $proj_home/infrastructure/api_gateway/target/demo.jar # 兼容所有sh脚本
 docker build -t aofeng/api-gateway .
 
-sudo mv $proj_home/service/cms/target/cms-*.jar $proj_home/service/cms/target/demo.jar # 兼容所有sh脚本
+sudo mv $proj_home/service/cms/target/cms-0.0.1-SNAPSHOT.jar $proj_home/service/cms/target/demo.jar # 兼容所有sh脚本
 docker build -t aofeng/cms .
 
-sudo mv $proj_home/service/edu/target/edu-*.jar $proj_home/service/edu/target/demo.jar # 兼容所有sh脚本
+sudo mv $proj_home/service/edu/target/edu-0.0.1-SNAPSHOT.jar $proj_home/service/edu/target/demo.jar # 兼容所有sh脚本
 docker build -t aofeng/edu .
 
-sudo mv $proj_home/service/order/target/order-*.jar  $proj_home/service/order/target/demo.jar # 兼容所有sh脚本
+sudo mv $proj_home/service/order/target/order-0.0.1-SNAPSHOT.jar  $proj_home/service/order/target/demo.jar # 兼容所有sh脚本
 docker build -t aofeng/order .
 
-sudo mv $proj_home/service/oss/target/oss-*.jar $proj_home/service/oss/target/demo.jar # 兼容所有sh脚本
+sudo mv $proj_home/service/oss/target/oss-0.0.1-SNAPSHOT.jar $proj_home/service/oss/target/demo.jar # 兼容所有sh脚本
 docker build -t aofeng/oss .
 
-sudo mv $proj_home/service/sms/target/sms-*.jar $proj_home/service/sms/target/demo.jar # 兼容所有sh脚本
+sudo mv $proj_home/service/sms/target/sms-0.0.1-SNAPSHOT.jar $proj_home/service/sms/target/demo.jar # 兼容所有sh脚本
 docker build -t aofeng/sms .
 
-sudo mv $proj_home/service/statistics/target/statistics-*.jar $proj_home/service/statistics/target/demo.jar # 兼容所有sh脚本
+sudo mv $proj_home/service/statistics/target/statistics-0.0.1-SNAPSHOT.jar $proj_home/service/statistics/target/demo.jar # 兼容所有sh脚本
 docker build -t aofeng/statistics .
 
-sudo mv $proj_home/service/user/target/user-*.jar $proj_home/service/user/target/demo.jar # 兼容所有sh脚本
+sudo mv $proj_home/service/user/target/user-0.0.1-SNAPSHOT.jar $proj_home/service/user/target/demo.jar # 兼容所有sh脚本
 docker build -t aofeng/user .
 
-sudo mv $proj_home/service/vod/target/vod-*.jar $proj_home/service/vod/target/demo.jar # 兼容所有sh脚本
+sudo mv $proj_home/service/vod/target/vod-0.0.1-SNAPSHOT.jar $proj_home/service/vod/target/demo.jar # 兼容所有sh脚本
 docker build -t aofeng/vod .
 
 echo "构建镜像"
